10f3b808d65600ce92002daf57d6e2757c7a7098
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2020-12-02 22:18:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,3 +23,20 @@ all_p = list(range(m))
 all_n = list(range(so_buoi))
 all_t = list(range(so_tiet))
 all_l = list(range(n))
+
+def enforce_class(L, P, B, T):
+    for p in all_p:
+        for b in all_b:
+            for t in all_t:
+                if (p,b,t)==(P,B,T):
+                    if p <= P+classes_len[C]: # nếu mà nó nằm trong khoảng số tiết của lớp đó
+                        return lc[(L,b,p,t)] == 1
+                    else: # nếu mà nó nằm ngoài khoảng số tiết của lớp đó mà giông p,b,t
+                        return lc[(L,b,p,t)] == 0
+                else: # khác p,b,t thì không được
+                    return return lc[(L,b,p,t)] == 0
+for l in all_l:
+    for p in all_p:
+        for b in all_b:
+            for t in all_t:
+                model.Add(enforce_class(l,p,b,t)).OnlyEnforceIf(lc[(l,p,b,t)]) # chỉ thêm constraint nếu mà biến lc[l,p,b,t]==1 
