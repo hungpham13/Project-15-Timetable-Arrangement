f1588421000a2b8873b2516f584052e3293d3ba6
hungpham13 <63634869+hungpham13@users.noreply.github.com>
2020-12-02 22:40:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-#Input
-f = open(r'data.txt','r')
-(n, m) =  (int(i) for i in f.readline().split())
-T = []
-S = []
-G = []
-D_G = []
-for c in range(n):
-    [so_tiet, gv, so_luong_hs] = (int(i) for i in f.readline().split())
-    T.append(so_tiet)
-    S.append(so_luong_hs)
-    G.append(gv)
-    if len(D_G)<gv:
-        D_G.append([c])
-    else:
-        D_G[gv-1].append(c)
-C = [int(i) for i in f.readline().split()]
-so_gv = len(D_G)
-so_buoi = 10
-so_tiet = 6
-all_gv = list(range(so_gv))
-all_p = list(range(m))
-all_b = list(range(so_buoi))
-all_t = list(range(so_tiet))
-all_l = list(range(n))
+def input(FileName):
+    f = open(FileName,'r')
+    (n, m) =  (int(i) for i in f.readline().split())
+    T,S,G,D_G,C = [],[],[],[],[]
+    for c in range(n):
+        [so_tiet, gv, so_luong_hs] = (int(i) for i in f.readline().split())
+        T.append(so_tiet)
+        S.append(so_luong_hs)
+        G.append(gv)
+        if len(D_G)<gv:
+            D_G.append([c])
+        else:
+            D_G[gv-1].append(c)
+    C = [int(i) for i in f.readline().split()]
+    return (n,m,T,S,G,D_G,C)
+
+FileName= 'data.txt'
+n,m,T,S,G,D_G,C = input(FileName)
+so_buoi,so_tiet= 10,6
+all_gv = range(len(D_G))
+all_p = range(m)
+all_b = range(so_buoi)
+all_t = range(so_tiet)
+all_l = range(n)
+print(T,S,G,D_G)
 
 #CP a.k.a or-tools
 
@@ -33,6 +33,20 @@ for l in all_l:
         for b in all_b:
             for t in all_t:
                 lc[(l, p, b, t]] = model.NewBoolVar('lc_l%ip%ib%it%i' %(l, p, b, t))
+                    
+#Constraint 1,2             
+for b in all_b:
+    for t in all_t:
+        for g in range(D_G):
+            model.Add(sum(sum(lc[(l,p,b,t)] for p in range(all_p)) \
+                          for l in D_G[g]) <= 1)           #2b
+        for p in all_p:
+            model.Add(sum(lc[(l,p,b,t)] for l in all_l)) #2a
+            for l in all_l:
+                if S[l] > C[p]:
+                    model.Add(lc[(l,p,b,t)] == 1)       #1
+
+#Constraint 3 - Vu
 def enforce_class(L, P, B, T):
     for p in all_p:
         for b in all_b:
