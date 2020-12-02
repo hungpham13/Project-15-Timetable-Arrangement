13583e89c61dd0c1b24f2f8343a9f4b076b2d0da
hungpham13 <tonic1307@gmail.com>
2020-12-02 22:03:35 +0700

    new

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+FileName= 'data.txt'
+def input(FileName):
+    """"""
+
+for b in all_b:
+    for t in all_t:
+        for g in range(D_G):
+            model.Add(sum(sum(lc[(l,p,b,t)] for p in range(all_p)) \
+                          for l in D_G[g]) <= 1)           #2b
+        for p in all_p:
+            model.Add(sum(lc[(l,p,b,t)] for l in all_l)) #2a
+            for l in all_l:
+                if S[l] > C[p]:
+                    model.Add(lc[(l,p,b,t)] == 1)       #1
+
+
+
 
