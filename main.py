4d013344fea225f898dd761b3d852a671dfeb27c
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2020-12-02 21:55:14 +0700

    Update main.py
    Input

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-
+#Input
+f = open(r'data.txt','r')
+(n, m) =  (int(i) for i in f.readline().split())
+T = []
+S = []
+G = []
+D_G = []
+for c in range(n):
+    [so_tiet, gv, so_luong_hs] = (int(i) for i in f.readline().split())
+    T.append(so_tiet)
+    S.append(so_luong_hs)
+    G.append(gv)
+    if len(D_G)<gv:
+        D_G.append([c])
+    else:
+        D_G[gv-1].append(c)
+C = f.readline().split()
+for i in range(m): #kinda dumb
+    C[i] = int(C[i])
+so_gv = len(D_G)
+so_buoi = 10
+so_tiet = 6
+all_gv = list(range(so_gv))
+all_p = list(range(m))
+all_n = list(range(so_buoi))
+all_t = list(range(so_tiet))
+all_l = list(range(n))
+print(T)
+print(S)
+print(G)
+print(D_G)
