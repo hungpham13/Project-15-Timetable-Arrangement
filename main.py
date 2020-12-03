aedfbd903e53951f8bd656864c737913e890e72a
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2020-12-03 10:14:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -54,15 +54,17 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
 
 
     def on_solution_callback(self):
-        if self._solution_count < self._so_loi_giai:
+        self._solution_count += 1  
+        if self._solution_count <= self._so_loi_giai:
             print('loi giai %i' % self._solution_count)
-            for l in range(self._n):
-                for p in range(self._m):
-                    for b in range(self._so_buoi):
-                        for p in range(self._so_tiet):
+            for p in range(self._m):
+                for b in range(self._so_buoi):
+                    for p in range(self._so_tiet):
+                        for l in range(self._n):
                             if self.Value(self._lc[(l,p,b,t)]):
-                                print(l,p,b,t) # print l,p,b,t iff lc ==1
-        self._solution_count += 1
+                                print(l,p,b,t)
+                                 
+
 
     def solution_count(self):
         return self._solution_count
@@ -73,42 +75,43 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
 #Constraint 1,2             
 for b in all_b:
     for t in all_t:
-        for g in range(D_G):
-            model.Add(sum(sum(lc[(l,p,b,t)] for p in range(all_p)) \
+        for g in all_gv:
+            model.Add(sum(sum(lc[(l,p,b,t)] for p in all_p) \
                           for l in D_G[g]) <= 1)           #2b
         for p in all_p:
-            model.Add(sum(lc[(l,p,b,t)] for l in all_l)) #2a
+            model.Add(sum(lc[(l,p,b,t)] for l in all_l) <= 1) #2a
             for l in all_l:
                 if S[l] > C[p]:
-                    model.Add(lc[(l,p,b,t)] == 1)       #1
+                    model.Add(lc[(l,p,b,t)] == 0)       #1
 
 #Constraint 3 - Vu
-def enforce_class(L, P, B, T):
+def enforce_class(l1, p1, b1, t1):
     for p in all_p:
         for b in all_b:
             for t in all_t:
-                if (p,b,t)==(P,B,T):
-                    if p <= P+classes_len[C]: # nếu mà nó nằm trong khoảng số tiết của lớp đó
-                        return lc[(L,p,b,t)] == 1
+                if (p,b,t)==(p1,b1,t1):
+                    if t <= t+T[l1]:     # nếu mà nó nằm trong khoảng số tiết của lớp đó
+                        return lc[(l1,p,b,t)] == 1
                     else: # nếu mà nó nằm ngoài khoảng số tiết của lớp đó mà giông p,b,t
-                        return lc[(L,p,b,t)] == 0
+                        return lc[(l1,p,b,t)] == 0
                 else: # khác p,b,t thì không được
-                    return lc[(L,p,b,t)] == 0
+                    return lc[(l1,p,b,t)] == 0
 for l in all_l:
     for p in all_p:
         for b in all_b:
             for t in all_t:
                 model.Add(enforce_class(l,p,b,t)).OnlyEnforceIf(lc[(l,p,b,t)]) # chỉ thêm constraint nếu mà biến lc[l,p,b,t]==1  
-                model.Add(sum(sum(sum(lc[(l,p,b,t)] for t in range(all_t))\
-                                                    for b in range(all_b))\
-                                                    for p in range(all_p))==T[c]) # đủ số tiết của lớp
+                model.Add(sum(sum(sum(lc[(l,p,b,t)] for t in all_t)\
+                                                    for b in all_b)\
+                                                    for p in all_p)==T[l]) # đủ số tiết của lớp
                 
                 
                 
 #Start solving and printing sols            
 solver = cp_model.CpSolver()
 solver.parameters.linearization_level = 0
-so_loi_giai = 5
+status = solver.Solve(model)
+so_loi_giai = 1
 solution_printer = SolutionPrinter(lc, n, m, so_buoi, so_tiet, so_loi_giai)
 solver.SearchForAllSolutions(model, solution_printer)
 
