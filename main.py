5f150c9a41d46ca9d53d8f55765dcecec58304b9
usb <tonic1307@gmail.com>
2020-12-04 13:56:24 +0700

    Hung

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ortools.sat.python import cp_model
 def input(FileName):
     f = open(FileName,'r')
     (n, m) =  (int(i) for i in f.readline().split())
@@ -34,7 +35,6 @@ for l in all_l:
             for t in all_t:
                 lc[(l, p, b, t)] = model.NewBoolVar('lc_l%ip%ib%it%i' %(l, p, b, t))
 
-
 # sol. printer TBD               
 class SolutionPrinter(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
@@ -68,7 +68,7 @@ class SolutionPrinter(cp_model.CpSolverSolutionCallback):
 #end printer
 
 
-#Constraint 1,2             
+#Constraint 1,2
 for b in all_b:
     for t in all_t:
         for g in all_gv:
@@ -80,38 +80,44 @@ for b in all_b:
                 if S[l] > C[p]:
                     model.Add(lc[(l,p,b,t)] == 0)       #1
 
-#Constraint 3 - Vu
-def enforce_class(l1, p1, b1, t1):
-    for p in all_p:
-        for b in all_b:
-            for t in all_t:
-                if (p,b,t)==(p1,b1,t1):
-                    if t <= t+T[l1]:     # nếu mà nó nằm trong khoảng số tiết của lớp đó
-                        return lc[(l1,p,b,t)] == 1
-                    else: # nếu mà nó nằm ngoài khoảng số tiết của lớp đó mà giông p,b,t
-                        return lc[(l1,p,b,t)] == 0
-                else: # khác p,b,t thì không được
-                    return lc[(l1,p,b,t)] == 0
+##Constraint 3 - Vu
+#def enforce_class(l1, p1, b1, t1):
+#    for p in all_p:
+#        for b in all_b:
+#            for t in all_t:
+#                if (p,b,t)==(p1,b1,t1):
+#                    if t <= t+T[l1]:     # nếu mà nó nằm trong khoảng số tiết của lớp đó
+#                        return lc[(l1,p,b,t)] == 1
+#                    else: # nếu mà nó nằm ngoài khoảng số tiết của lớp đó mà giông p,b,t
+#                        return lc[(l1,p,b,t)] == 0
+#                else: # khác p,b,t thì không được
+#                    return lc[(l1,p,b,t)] == 0
 
 for l in all_l:
-    for p in all_p:
-        for b in all_b:
-            for t in all_t:
-                model.Add(enforce_class(l,p,b,t)).OnlyEnforceIf(lc[(l,p,b,t)]) # chỉ thêm constraint nếu mà biến lc[l,p,b,t]==1  
-                model.Add(sum(sum(sum(lc[(l,p,b,t)] for t in all_t)\
-                                                    for b in all_b)\
-                                                    for p in all_p)==T[l]) # đủ số tiết của lớp
+#    for p in all_p:
+#        for b in all_b:
+#            for t in all_t:
+#                model.Add(enforce_class(l,p,b,t)).OnlyEnforceIf(lc[(l,p,b,t)]) # chỉ thêm constraint nếu mà biến lc[l,p,b,t]==1  
+   model.Add(sum(sum(sum(lc[(l,p,b,t)] for t in all_t)\
+                                                  for b in all_b)\
+                                                  for p in all_p)==T[l]) # đủ số tiết của lớp
 
 #Constraint 3 - Hung
 for l in all_l:
     for t in all_t:
-        model.Add(sum(sum(lc[(l,p,b,t)] for p in all_p) for b in all_b) <=1) #3a
+        model.Add(sum(sum(lc[(l,p,b,t)] for p in all_p) for b in all_b) <= 1) #3a
 
 
 def valid_list(l_S,pivot):
     '''list do dai >= 1, chua cac S_i'''
-    if set(l_S) == {0}: return True
-    if pivot not in l_S: return False
+    if sum(l_S) == 0: return True
+    has_pivot = False
+    for i in range(len(l_S)):
+        if l_S[i] == pivot:
+            has_pivot = True
+    if not has_pivot:
+        return False
+
     up = True
     for i in range(len(l_S)):
         if i != 0 and l_S[i] + l_S[i-1] != 0:
@@ -135,15 +141,17 @@ for l in all_l:
 
 #Start solving and printing sols            
 solver = cp_model.CpSolver()
-solver.parameters.linearization_level = 0
 status = solver.Solve(model)
-so_loi_giai = 1
-solution_printer = SolutionPrinter(lc, n, m, so_buoi, so_tiet, so_loi_giai)
-solver.SearchForAllSolutions(model, solution_printer)
+for b in all_b:
+    print('Buoi',b)
+    for l in all_l:
+        for p in all_p:
+            for t in all_t:
+                if solver.Value(lc[(l,p,b,t)]) == 1:
+                    print('Lop',l,'hoc phong',p,'tiet',t)
 
-# print()
-# print('Statistics')
-# print('  - conflicts       : %i' % solver.NumConflicts())
-# print('  - branches        : %i' % solver.NumBranches())
-# print('  - wall time       : %f s' % solver.WallTime())
-# print('  - solutions found : %i' % solution_printer.solution_count())
+print(status)
+print('Statistics')
+print('  - conflicts       : %i' % solver.NumConflicts())
+print('  - branches        : %i' % solver.NumBranches())
+print('  - wall time       : %f s' % solver.WallTime())
