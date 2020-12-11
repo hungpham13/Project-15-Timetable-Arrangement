0524a1b8ec34814b16fa376b8a638fda7aa0617a
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2020-12-11 18:25:59 +0700

    Sửa lại randomize_data và thêm description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ortools.sat.python import cp_model
-from randm import randint
+from random import randint
 
 def input(FileName,toPrint):
     f = open(FileName,'r')
@@ -20,17 +20,17 @@ def input(FileName,toPrint):
 def randomize_data(FileName):
     rnum_hsmax = 0
     f = open(FileName, "w")
-    n, m = random.randint(5, 25), random.randint(2, 5)
+    n, m = randint(5, 25), randint(2, 5) #random số lớp (5-25) và số phòng (2-5)
     f.write(str(n) + ' ' + str(m) + '\n')
     for l in range(n):
-        rnum_hs = random.randint(30, 50)
-        f.write(str(random.randint(1, 6)) + ' ' + str(random.randint(1, 5)) + ' ' + str(rnum_hs) + '\n')
+        rnum_hs = randint(30, 50) #random số học sinh (30-50)
+        f.write(str(randint(1, 6)) + ' ' + str(randint(1, 5)) + ' ' + str(rnum_hs) + '\n') #random độ dài (1-6) và giáo viên (1-5), học sinh rồi viết vào file 
         if rnum_hs > rnum_hsmax: #đảm bảo là luôn có phòng chứa được lớp nhiều học sinh nhất
             rnum_hsmax = rnum_hs
     room = ''
     for i in range(m - 1):
-        room += str(random.randint(30, 50)) + ' '
-    f.write(room + str(rnum_hsmax))
+        room += str(randint(30, 50)) + ' ' #random số chỗ của m-1 phòng (30-50)
+    f.write(room + str(rnum_hsmax))#viết vào file số chỗ của m-1 phòng và phòng cuối (Phòng cuối sẽ luôn có số chỗ bằng số lớp nhiều học sinh nhất)
 
 
 FileName= 'data.txt'
