00672ee44e0b445483acc0445ec3df5f11ea20f9
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2020-12-13 13:35:30 +0700

    Thêm Sort_list

--- main.py
+++ main.py
@@ -179,6 +179,10 @@ def test_Ortools():
 #Heuristic#
 ###########
 
+def sort_list(list1, list2):
+    zipped_pairs = zip(list2, list1)
+    z = [x for _, x in sorted(zipped_pairs)]
+    return z
 
 def print_sol(target):
     lop_da_duoc_xep = []
